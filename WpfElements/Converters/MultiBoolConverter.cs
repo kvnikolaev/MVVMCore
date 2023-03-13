6208fc0f97e66d5e93b7f823ec0c27ad0fdf7cf1
Николаев Кирилл <kvnikolaev3@gmail.com>
2023-03-13 17:05:44 +0300

    Loading animation

---
+++ WpfElements/Converters/MultiBoolConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfElements
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class MultiBoolConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = false;
+            switch (parameter.ToString())
+            {
+                case "XOR":
+                    foreach (var value in values)
+                    {
+                        result |= (bool)value;
+                    }
+                    return !result;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
