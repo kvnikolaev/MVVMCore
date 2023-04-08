f44e179d7164411614d29faf9fe93915a5dddd6d
Николаев Кирилл <kvnikolaev3@gmail.com>
2023-04-08 19:32:13 +0300

    Squircle preparation

---
+++ WpfElements/Shapes/Squircle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfElements.Shapes
+{
+    //public class SquircleGeometry : Geometry
+    //{
+    //    //public SquircleGeometry() { }
+
+    //    public override bool IsEmpty()
+    //    {
+    //        return false;
+    //    }
+
+    //    public override bool MayHaveCurves()
+    //    {
+    //        return true;
+    //    }
+
+    //    protected override Freezable CreateInstanceCore()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
